5822acf3b6e1545a4386ae48dda907d291b34fc7
Janice <janiceralecha@gmail.com>
2022-09-19 18:20:04 -0500

    Add Home component.

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <section id="home">
+            <h1 style={{ color: "firebrick" }}>
+                Welcome to Postagram
+            </h1>
+        </section>
+    );
+}
+
+export default Home;
