9a765bacb071644c975c55f2dbf03715b8c9db24
Janice <janiceralecha@gmail.com>
2022-09-22 20:29:12 -0500

    Add useEffect and useEffect hook on app component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
@@ -6,17 +6,27 @@ import Header from "./Header"
 import PostCardForm from "./PostCardForm";
 import SearchPostcard from "./SearchPostcard";
 import Footer from "./Footer"
+import PostCard from './PostCard';
 
 
 
 
 function App() {
+    const [postcards, setPostcards] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3001/postcards")
+        .then(response => response.json())
+        .then(postcards => setPostcards(postcards))
+      }, [])
+      
     
   return (
       <div>
           
           <Header />
           <NavBar />
+          <PostCard />
           <Switch>
               
               <Route exact path="/postcardform">
