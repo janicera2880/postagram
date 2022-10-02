75180c28dea287beb1ea311a02440463f2eab436
Janice <janiceralecha@gmail.com>
2022-10-01 19:29:55 -0500

    fix Css styling and border

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,26 +6,29 @@ function Home() {
   
   function handleSignUp(event) {
     event.preventDefault()
-    console.log(event.target.username.value)  
+   // console.log(event.target.username.value)  
   }
   
   return (
-    <form onSubmit={handleSignUp}>
-      <h1>Create an Account</h1>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
+    <div>
+        <h1>A home for a new generation of travelers, photographers and adventure seekers.</h1>
+        <h2>Join our community and register below for Free.</h2>
+ 
+        <form onSubmit={handleSignUp}>
+        <h1>Create an Account</h1>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
 
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
 
-      <label htmlFor="type">Account Type</label>
-      <select id="type">
-        <option value="free">Free</option>
-        <option value="pro">VIP</option>
-      </select>
+        <label>
+        Get Our Newsletter! <input type="checkbox" id="newsletter" />
+        </label>
 
-      <input type="submit" value="Sign Up" />
-    </form>
+        <input type="submit" value="Sign Up" />
+        </form>
+    </div>
   );
 }
 
