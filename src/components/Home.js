94f61d8634a0d7f498d864646ccaa01ff5ada1ac
Janice <janiceralecha@gmail.com>
2022-10-09 11:30:40 -0500

    Add comment in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ function Home({ isLoggedIn }) {
     <div>
       <h2>WELCOME FELLOW POSTAGRAMERS</h2>
       <h1>A home for a new generation of travelers, photographers and adventure seekers.</h1>
+      <p>We believe that the most enjoyable moments are those that are filled with spontaneous activities and experiences.  So, whether you are looking for vacation getaway, to explore new city / country, or to find the perfect companions to travel the world with, we have got you covered!</p>
       <h1>Browse popular destinations all over the world in the Search link...</h1>
       <h1>and feel free to share your own experience and fill up the Upload form.</h1>
       <img id="homeImg" src={homeImg} alt="homeImg" className="center"/>
