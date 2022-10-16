508ca56db3e746b3bccc4fedd5d451296846742d
Janice <janiceralecha@gmail.com>
2022-10-15 22:27:35 -0500

    Add comments to PostcardPage component

--- src/components/PostcardPage.js
+++ src/components/PostcardPage.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import PostcardList from "./PostcardList";
 import SearchPostcard from "./SearchPostcard";
 
+//Here declare useState hook using const
+//First argument state variable are postcards and search then named the function setPostcard and setSearch
+//passing empty string and empty array as initial state to the useState hook
+
 
 function PostcardPage() {
 
@@ -9,21 +13,23 @@ function PostcardPage() {
     const [postcards, setPostcards] = useState([]);
     const [search, setSearch] = useState("");
     
-    
+    //useEffect hook to fetch some data from API
     useEffect(() => {
       fetch("http://localhost:3001/postcards")
         .then((res) => res.json())
         .then((data) => {
+           // setting state in the useEffect callback
           setPostcards(data);
         });
-    }, []);
+    }, []);//also pass in an empty array of dependencies as a second argument this way side effect will
+     //only run first time the component renders
    
     function handleChangeSearch(currentSearch) {
       setSearch(currentSearch)
      
     }
 
-  
+  //In Searching by country, use filter method to remove and create new array to also covert string lowercase
     const shownPostcards = postcards.filter((postcard) => {
   
 
